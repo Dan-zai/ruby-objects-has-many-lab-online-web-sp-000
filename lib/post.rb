f0549c78cb39d4a36a4c5f0cb51e147c98bab6e7
Daniel Cowpertwait <danielcowpertwait@gmail.com>
2020-02-19 06:13:24 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,22 @@
+class Post 
+  
+  attr_accessor :title, :author
+  
+  @@all = []
+  
+  def initialize(title) 
+    @name = name
+    @@all << self
+  end 
+  
+  def self.all
+    @@all
+  end
+  
+  def artist_name
+    if artist
+      artist.name 
+    end 
+  end 
+  
+end 
